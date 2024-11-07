ed17a2e969065fdbbb25db6994587d954c5d8f3f
Konstantin Pavlov <{ID}+{username}@users.noreply.github.com>
2024-11-07 21:23:33 +0200

    Add background color drawables
    Introduced BLUE_BACKGROUND and RED_BACKGROUND using the new createBackground function, which generates a Drawable instance from a given color. This function uses Pixmap and Texture to define the background and ensures resources are disposed of properly.

--- core/src/main/kotlin/me/kpavlov/elven/Colors.kt
+++ core/src/main/kotlin/me/kpavlov/elven/Colors.kt
@@ -1,6 +1,11 @@
 package me.kpavlov.elven
 
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.Pixmap
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
 
 /** This represents a rich, deep green often seen in forested areas. */
 val LUSH_FOREST_GREEN = Color(0.133333f, 0.545098f, 0.133333f, 1f)
@@ -16,3 +21,15 @@ val MOSSY_GREEN = Color(0.333333f, 0.419608f, 0.184314f, 1f)
  * foliage.
  */
 val EARTHY_BROWN = Color(0.396078f, 0.262745f, 0.129412f, 1f)
+
+val BLUE_BACKGROUND = createBackground(Color.BLUE)
+val RED_BACKGROUND = createBackground(Color.RED)
+
+private fun createBackground(color: Color): Drawable {
+    val pixmap = Pixmap(1, 1, Pixmap.Format.RGBA8888)
+    pixmap.setColor(color)
+    pixmap.fill()
+    val texture = Texture(pixmap)
+    pixmap.dispose()
+    return TextureRegionDrawable(TextureRegion(texture))
+}
